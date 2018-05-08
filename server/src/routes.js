530de8a0f8d51f9773433a2560ae4fca91e0ca96
Sean Stevens <sstevens88@gmail.com>
2018-05-08 12:10:33 -0700

    added client changes

--- server/src/routes.js
+++ server/src/routes.js
@@ -7,4 +7,9 @@ module.exports = (app) => {
     AuthenticationControllerPolicy.register,
     AuthenticationController.register
   )
+
+  app.post(
+    '/login',
+    AuthenticationController.login
+  )
 }
